9e016d53047f210b59185ff36d3011849714a594
Fyodor Danilov <danilovfa@protonmail.com>
2023-04-21 23:40:05 +0300

    Remove initial code, added viewbinding delegate

---
+++ app/src/main/java/com/danilovfa/pokemontracker/ui/DetailsFragment.kt
@@ -0,0 +1,26 @@
+package com.danilovfa.pokemontracker.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import by.kirich1409.viewbindingdelegate.CreateMethod
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.danilovfa.pokemontracker.databinding.FragmentDetailsBinding
+
+class DetailsFragment : Fragment() {
+
+    private val binding: FragmentDetailsBinding by viewBinding(createMethod = CreateMethod.INFLATE)
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
--- app/src/main/java/com/danilovfa/pokemontracker/ui/MainActivity.kt
+++ app/src/main/java/com/danilovfa/pokemontracker/ui/MainActivity.kt
@@ -1,80 +1,36 @@
 package com.danilovfa.pokemontracker.ui
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
-import androidx.room.Room
+import by.kirich1409.viewbindingdelegate.CreateMethod
+import by.kirich1409.viewbindingdelegate.viewBinding
 import com.danilovfa.pokemontracker.R
-import com.danilovfa.pokemontracker.data.local.database.PokemonDetailsDatabase
-import com.danilovfa.pokemontracker.data.local.database.PokemonPageDatabase
 import com.danilovfa.pokemontracker.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var binding: ActivityMainBinding
-    private val dbPage by lazy {
-        Room.databaseBuilder(
-            applicationContext,
-            PokemonPageDatabase::class.java,
-            "pokemons.db"
-        ).build()
-    }
-//    val pageDao = dbPage.pokemonPageDao
-    private val dbDetails by lazy {
-        Room.databaseBuilder(
-            applicationContext,
-            PokemonDetailsDatabase::class.java,
-            "details.db"
-        ).build()
-    }
-//    val detailsDao = dbDetails.pokemonDetailsDao
+    private val binding: ActivityMainBinding by viewBinding(createMethod = CreateMethod.INFLATE)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         WindowCompat.setDecorFitsSystemWindows(window, false)
         super.onCreate(savedInstanceState)
-
-        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         setSupportActionBar(binding.toolbar)
-
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment_container) as NavHostFragment
+        val navController = navHostFragment.navController
         appBarConfiguration = AppBarConfiguration(navController.graph)
         setupActionBarWithNavController(navController, appBarConfiguration)
-
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAnchorView(R.id.fab)
-                .setAction("Action", null).show()
-        }
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        val navController = findNavController(R.id.fragment_container)
         return navController.navigateUp(appBarConfiguration)
                 || super.onSupportNavigateUp()
     }
