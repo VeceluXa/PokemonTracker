ce6f55ae90309b0cc935f4d1ce85d239458dc060
Fyodor Danilov <danilovfa@protonmail.com>
2023-04-23 02:20:17 +0300

    Add viewmodels

--- app/src/main/java/com/danilovfa/pokemontracker/presentation/view/DetailsFragment.kt
+++ app/src/main/java/com/danilovfa/pokemontracker/presentation/view/DetailsFragment.kt
@@ -5,13 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import by.kirich1409.viewbindingdelegate.CreateMethod
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.danilovfa.pokemontracker.databinding.FragmentDetailsBinding
+import com.danilovfa.pokemontracker.presentation.viewmodel.DetailsViewModel
 
 class DetailsFragment : Fragment() {
 
     private val binding: FragmentDetailsBinding by viewBinding(createMethod = CreateMethod.INFLATE)
+    private lateinit var viewModel: DetailsViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,5 +25,6 @@ class DetailsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProvider(this)[DetailsViewModel::class.java]
     }
 }
--- app/src/main/java/com/danilovfa/pokemontracker/presentation/view/PageFragment.kt
+++ app/src/main/java/com/danilovfa/pokemontracker/presentation/view/PageFragment.kt
@@ -5,13 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import by.kirich1409.viewbindingdelegate.CreateMethod
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.danilovfa.pokemontracker.databinding.FragmentPageBinding
+import com.danilovfa.pokemontracker.presentation.viewmodel.PageViewModel
 
 class PageFragment : Fragment() {
 
     private val binding: FragmentPageBinding by viewBinding(createMethod = CreateMethod.INFLATE)
+    private lateinit var viewModel: PageViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,5 +26,6 @@ class PageFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProvider(this)[PageViewModel::class.java]
     }
 }
