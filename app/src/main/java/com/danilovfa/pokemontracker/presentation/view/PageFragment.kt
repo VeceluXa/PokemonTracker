8a72a0a3adeac1e41631ff067f8dc27ee0e265b2
Fyodor Danilov <danilovfa@protonmail.com>
2023-04-26 02:31:32 +0300

    Add on item click navigation

--- app/src/main/java/com/danilovfa/pokemontracker/presentation/view/PageFragment.kt
+++ app/src/main/java/com/danilovfa/pokemontracker/presentation/view/PageFragment.kt
@@ -6,15 +6,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
 import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
+import androidx.navigation.fragment.findNavController
 import androidx.paging.LoadState
 import androidx.paging.map
 import by.kirich1409.viewbindingdelegate.CreateMethod
 import by.kirich1409.viewbindingdelegate.viewBinding
+import com.danilovfa.pokemontracker.R
 import com.danilovfa.pokemontracker.databinding.FragmentPageBinding
 import com.danilovfa.pokemontracker.domain.usecase.GetPokemonByPageUseCase
 import com.danilovfa.pokemontracker.presentation.adapters.PokemonLoaderAdapter
@@ -26,7 +29,7 @@ import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
-class PageFragment : Fragment() {
+class PageFragment : Fragment(), PokemonPageAdapter.OnItemClickListener {
 
     private val binding: FragmentPageBinding by viewBinding(createMethod = CreateMethod.INFLATE)
     private val viewModel: PageViewModel by viewModels()
@@ -58,6 +61,13 @@ class PageFragment : Fragment() {
                 adapter.submitData(it)
             }
         }
+
+        adapter.setOnItemClickLister(this)
+    }
+
+    override fun onItemClick(itemId: Int) {
+        val bundle = bundleOf("id" to itemId)
+        findNavController().navigate(R.id.action_PageFragment_to_DetailsFragment, bundle)
     }
 
 
