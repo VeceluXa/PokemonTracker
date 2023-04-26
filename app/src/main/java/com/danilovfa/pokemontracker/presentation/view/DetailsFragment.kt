6aa5c44e31a504c49c6ef4d646569dc28ac5a73c
Fyodor Danilov <danilovfa@protonmail.com>
2023-04-26 04:49:46 +0300

    Fix details ui

--- app/src/main/java/com/danilovfa/pokemontracker/presentation/view/DetailsFragment.kt
+++ app/src/main/java/com/danilovfa/pokemontracker/presentation/view/DetailsFragment.kt
@@ -8,7 +8,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.lifecycleScope
 import by.kirich1409.viewbindingdelegate.CreateMethod
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.bumptech.glide.Glide
@@ -17,10 +16,6 @@ import com.danilovfa.pokemontracker.databinding.FragmentDetailsBinding
 import com.danilovfa.pokemontracker.domain.model.PokemonDetails
 import com.danilovfa.pokemontracker.presentation.viewmodel.DetailsViewModel
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-import java.util.*
 
 @AndroidEntryPoint
 class DetailsFragment : Fragment() {
@@ -46,10 +41,10 @@ class DetailsFragment : Fragment() {
         Log.d("Details", "onViewCreated: $id")
     }
 
-    fun setDetails(details: PokemonDetails) {
+    private fun setDetails(details: PokemonDetails) {
         binding.textDetailsName.text = details.name.replaceFirstChar { it.uppercaseChar() }
-        binding.textHeight.text = getString(R.string.height, details.heightInCm)
-        binding.textWeight.text = getString(R.string.weight, details.weightInKg)
+        binding.textHeight.text = getString(R.string.height, details.heightInDm * 10)
+        binding.textWeight.text = getString(R.string.weight, details.weightInHg.toDouble() / 10)
         Glide.with(this)
             .asBitmap()
             .load(details.sprite)
@@ -59,14 +54,27 @@ class DetailsFragment : Fragment() {
         details.types.forEach {type ->
             addType(type)
         }
+
+        setVisible()
     }
 
-    fun addType(type: String) {
+    private fun setVisible() {
+        with (binding) {
+            progressBarDetails.visibility = View.GONE
+            textDetailsName.visibility = View.VISIBLE
+            textWeight.visibility = View.VISIBLE
+            textWeightLabel.visibility = View.VISIBLE
+            textHeight.visibility = View.VISIBLE
+            textHeightLabel.visibility = View.VISIBLE
+        }
+    }
+
+    private fun addType(type: String) {
         val layout = LayoutInflater.from(requireContext())
             .inflate(R.layout.pokemon_type_item_layout, binding.typesLayout, false)
 
         val textView = layout.findViewById<TextView>(R.id.textType)
-        textView.text = type
+        textView.text = type.replaceFirstChar { it.uppercaseChar() }
 
         binding.typesLayout.addView(layout)
     }
